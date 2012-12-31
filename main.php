71b4c2499c2b735845c31f9b1aeaac4c2e9753b7
C1D <c1d@mypin.im>
2012-12-31 23:11:20 +0400

    ADDED DETECT_MOBILE FUNCTION
    This function detects if the users is using a mobile device!

--- main.php
+++ main.php
@@ -40,4 +40,12 @@ function email_html($from, $to, $subject, $msg){
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 	mail($to, $subject, $msg, $headers,'-f'.$from);
 }
+//This function detects if the users is using a mobile device
+function detect_mobile(){
+	if(preg_match('/(alcatel|amoi|android|avantgo|blackberry|benq|cell|cricket|docomo|elaine|htc|iemobile|iphone|ipad|ipaq|ipod|j2me|java|midp|mini|mmp|mobi|motorola|nec-|nokia|palm|panasonic|philips|phone|playbook|sagem|sharp|sie-|silk|smartphone|sony|symbian|t-mobile|telus|up\.browser|up\.link|vodafone|wap|webos|wireless|xda|xoom|zte)/i', $_SERVER['HTTP_USER_AGENT'])){
+		return true;
+	}else{
+		return false;
+	}
+}
 ?>
